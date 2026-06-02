123ca9837bcce87bd6b53a50c1258b7dc882dcf4
Nicolas Nomi <dichava190@gmail.com>
2026-06-02 09:10:36 -0300

    02/06/2026

---
+++ LOPAL/NicolasNomi/Aulas/aula1.py
@@ -0,0 +1,14 @@
+# Comandos Python
+
+print("Olá, Mundo!")
+print("Olá Bruno")
+
+print("Olá Mundo \n")
+print("Meu hobbie é \n")
+print("\n Gosto de ouvir música e \n vou tentar ouvir GHOST \n")
+
+print("Bem-Vindo ao Sistema \n")
+print("Digite a opção desejada: \n")
+print("Opção 1: Estudar Python \n")
+print("Opção 2: Ouvir Ghost \n")
+print("Opção 3: Encerrar sistema \n")
